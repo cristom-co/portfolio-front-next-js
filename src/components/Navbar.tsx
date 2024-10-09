d5287de6e112804e7ebe8e8dc76d94de946b5ab5
cristhian <cristhiantombe@gmail.com>
2024-10-09 09:12:13 -0500

    remove all

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,37 +1,7 @@
-"use client";
-
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-
 const Navbar = () => {
-  const pathname = usePathname();
 
   return (
-    <div className="flex flex-row gap-4 pb-5">
-        <Link
-          className={`link ${pathname === "/" ? "text-green-500" : ""}`}
-          href="/"
-        >
-          Home
-        </Link>
-        <Link
-          className={`link ${pathname === "/about" ? "text-green-500" : ""}`}
-          href="/about"
-        >
-          About
-        </Link>
-        <Link
-          className={`link ${pathname === "/service" ? "text-green-500" : ""}`}
-          href="/service"
-        >
-          Service
-        </Link>
-        <Link
-          className={`link ${pathname === "/contact" ? "text-green-500" : ""}`}
-          href="/contact"
-        >
-          Contact
-        </Link>
+    <div> 
     </div>
   );
 };
