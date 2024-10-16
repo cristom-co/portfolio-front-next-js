729bfd45ffdcd8f34383792522e45d50b4e6d80a
cristhian <cristhiantombe@gmail.com>
2024-10-16 11:52:52 -0500

    slider horizontal

--- src/app/components/commons/SliderImages.tsx
+++ src/app/components/commons/SliderImages.tsx
@@ -1,13 +1,33 @@
 import Image from 'next/image';
-  
+
 const SliderImages = () => {
-  return <div>
-    <Image 
-        src="/images/product1.png" // Ruta de la imagen
-        alt="Descripción de la imagen" // Texto alternativo
-        width={600} // Ancho de la imagen
-        height={400} // Alto de la imagen
-      />
+
+  const portfolio = [
+    {
+      image: '/images/product1.png',
+      description: 'Descripción de la imagen'
+    },
+    {
+      image: '/images/product1.png',
+      description: 'Descripción de la imagen'
+    }
+  ];
+
+  return <div className='slider-wrapper'>
+    <Image className='rotate-90' src="/icons/arrow-down.png" alt="arrow-left" width={15} height={15} />
+    <div className="slider-container">
+      {portfolio.map((item, index) => (
+        <div className="slider-item" key={index}>
+          <Image
+            src={item.image} // Ruta de la imagen
+            alt={item.description} // Texto alternativo
+            width={300} // Ancho de la imagen
+            height={400} // Alto de la imagen
+          />
+        </div>
+      ))}
+    </div>
+    <Image className='-rotate-90' src="/icons/arrow-down.png" alt="arrow-right" width={15} height={15} />
   </div>;
 };
 
