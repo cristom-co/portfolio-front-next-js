af19492adbcf5a0fb9da9f46bf853431e94142d9
cristhian <cristhiantombe@gmail.com>
2024-10-17 20:01:26 -0500

    responsive desktop

--- src/app/components/Portfolio.tsx
+++ src/app/components/Portfolio.tsx
@@ -4,7 +4,7 @@ import TitleSection from "./commons/TitleSection";
 import { portafolio } from "../data/portafolio";
 
 const Portfolio = () => {
-  return <div className="sectionSite">
+  return <div className="sectionSite ">
     <TitleSection title="Portfolio" description="I have successfully collaborated with diverse industries, including advertising, finance, education, and travel." />
     <Slider items={portafolio} />
   </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import Head from "next/head";
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -16,6 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <body className={`${inter.className} p-5`}>
         {children}
       </body>
