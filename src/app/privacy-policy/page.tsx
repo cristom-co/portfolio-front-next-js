30d5a719c1e3dff1e5b138179f2ca83416450d19
Cristhian Tombe <cristhiantombe@gmail.com>
2025-03-09 12:24:17 -0500

    privacy policy

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,30 @@
+export default function PrivacyPolicy() {
+  return (
+    <div className="sm:w-full md:w-3/4 lg:w-2/3 xl:w-1/2 2xl:w-1/3 mx-auto">
+      <h1>Política de Privacidad</h1>
+      <p>
+      Esta política de privacidad describe cómo manejamos y protegemos su información personal en relación con el uso de nuestros servicios. Nos comprometemos a proteger su privacidad y a garantizar que su información personal esté segura.
+      </p>
+      <h2>Recopilación de Información</h2>
+      <p>
+      Recopilamos información personal que usted nos proporciona directamente, como su nombre, dirección de correo electrónico y cualquier otra información que decida compartir con nosotros. También podemos recopilar información automáticamente a través del uso de cookies y tecnologías similares.
+      </p>
+      <h2>Uso de la Información</h2>
+      <p>
+      Utilizamos la información recopilada para proporcionar, mantener y mejorar nuestros servicios, así como para comunicarnos con usted. No compartimos su información personal con terceros, excepto cuando sea necesario para cumplir con la ley o proteger nuestros derechos.
+      </p>
+      <h2>Seguridad de la Información</h2>
+      <p>
+      Implementamos medidas de seguridad adecuadas para proteger su información personal contra el acceso no autorizado, la alteración, divulgación o destrucción. Sin embargo, no podemos garantizar la seguridad absoluta de su información.
+      </p>
+      <h2>Cambios en la Política de Privacidad</h2>
+      <p>
+      Nos reservamos el derecho de actualizar esta política de privacidad en cualquier momento. Le notificaremos sobre cualquier cambio publicando la nueva política en nuestro sitio web. Le recomendamos revisar esta política periódicamente para estar informado sobre cómo protegemos su información.
+      </p>
+      <h2>Contacto</h2>
+      <p>
+      Si tiene alguna pregunta o inquietud sobre nuestra política de privacidad, no dude en contactarnos a través de la información de contacto proporcionada en nuestro sitio web.
+      </p>
+    </div>
+  );
+}
